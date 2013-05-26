f64d7d42acd502c9a0b5438bdd692d31bd5fb476
Michele Schimd <michele.schimd@gmail.com>
2013-05-26 20:26:22 +0200

    Android Target is now 2.2, added time constants, added string-to-date method

--- src/com/skimmy/androidutillibrary/time/DateAndTimeHelper.java
+++ src/com/skimmy/androidutillibrary/time/DateAndTimeHelper.java
@@ -1,9 +1,12 @@
 package com.skimmy.androidutillibrary.time;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
+
+import android.util.Log;
 
 /**
  * 
@@ -15,12 +18,15 @@ import java.util.TimeZone;
  */
 public class DateAndTimeHelper {
 
+	public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
+
 	private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
-			"yyyy-MM-dd HH:mm:ss.SSS", Locale.US);
+			DATE_TIME_FORMAT, Locale.US);
 
 	/**
-	 * This method returns the current timestamp of the UTC timezone. The returned
-	 * string has the form 'YYYY-MM-DD HH:mm:ss.SSS' 
+	 * This method returns the current timestamp of the UTC timezone. The
+	 * returned string has the form 'YYYY-MM-DD HH:mm:ss.SSS' defined in
+	 * {@link #DATE_TIME_FORMAT}
 	 * 
 	 * @return a string representing the current UTC timestamp
 	 */
@@ -28,5 +34,27 @@ public class DateAndTimeHelper {
 		simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 		String gmtTime = simpleDateFormat.format(new Date());
 		return gmtTime;
+	}
+
+	/**
+	 * This method parses a String object and returns the corresponding Date
+	 * object. If <code>date</code> does not conform to the format define by
+	 * {@link #DATE_TIME_FORMAT} the method returns <code>null</code> and prints
+	 * the corresponding error message into the system log.
+	 * 
+	 * @param date
+	 *            A {@link String} representing a UTC date and timestamp
+	 * @return A {@link Date} object obtained from the input parameter or null
+	 *         if it does not conform to the defined format
+	 */
+	public Date getUTCDateFromString(String date) {
+		try {
+			return simpleDateFormat.parse(date);
+		} catch (ParseException e) {
+			Log.e(DateAndTimeHelper.class.getName(), e.getMessage());
+			Log.v(DateAndTimeHelper.class.getName(), e.getStackTrace()
+					.toString());
+		}
+		return null;
 	}
 }
---
+++ src/com/skimmy/androidutillibrary/time/TimeConstants.java
@@ -0,0 +1,17 @@
+package com.skimmy.androidutillibrary.time;
+
+public class TimeConstants {
+
+	// some useful constants
+	public static final int MILLISECONDS_PER_SECOND = 1000;
+	public static final int SECONDS_PER_MINUTE = 60;
+	public static final int MILLISECONDS_PER_MINUTE = SECONDS_PER_MINUTE
+			* MILLISECONDS_PER_SECOND;
+	public static final int MINUTES_PER_HOUR = 60;
+	public static final int SECONDS_PER_HOUR = SECONDS_PER_MINUTE
+			* MINUTES_PER_HOUR;
+	public static final int HOURS_PER_DAY = 24;
+	public static final int MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
+	public static final int SECONDS_PER_DAY = MINUTES_PER_DAY * SECONDS_PER_MINUTE;
+
+}
